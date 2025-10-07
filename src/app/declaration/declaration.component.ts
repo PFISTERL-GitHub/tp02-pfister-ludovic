b817081fff7c5164363b7d7d3eed484751f518ec
PFISTER Ludovic <ludovicp2005@gmail.com>
2025-10-07 19:58:31 +0200

    Initial commit

---
+++ src/app/declaration/declaration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-declaration',
+  imports: [],
+  templateUrl: './declaration.component.html',
+  styleUrl: './declaration.component.css'
+})
+export class DeclarationComponent {
+
+}
---
+++ src/app/recap/recap.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recap',
+  imports: [],
+  templateUrl: './recap.component.html',
+  styleUrl: './recap.component.css'
+})
+export class RecapComponent {
+
+}
